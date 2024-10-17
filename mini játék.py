95a83c597486f77444aedeee14567efe5a84157d
Székely Szabolcs Attila (aka RTS) <72965662+szabiszekely@users.noreply.github.com>
2024-10-17 12:44:42 +0200

    Add files via upload

--- mini játék.py
+++ mini játék.py
@@ -4,13 +4,22 @@ class Warrior:
         self.health = health
         self.attack_power = attack_power
     
-    def attacj(self,other_warior):
-        other_warior.health -=  self.attack_power
+    def attack(self,other_warior):
+        other_warior -=  self.attack_power
         
-    def is_alive(self,alive):
+    def is_alive(self):
         if self.health <= 0:
-            alive = False
+            return False
         else:
-            alive = True
-            
-            
+            return True
+        
+
+harcos_1 = Warrior("Bukvaktás",20,3)
+harcos_2 = Warrior("Terbungos",22,2)
+igaz_1 = harcos_1.is_alive()
+igaz_2 = harcos_2.is_alive()
+while igaz_1 == True or igaz_2 == True:
+    print("rrrrr")
+    print(harcos_1.attack(harcos_2.health))
+    
+    
